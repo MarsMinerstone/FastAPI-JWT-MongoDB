5b83b92864df86f6af57c574fa61a982b581ab68
MarsMinerstone <magic777dance@mail.ru>
2022-05-24 12:45:40 +0700

    all refactored
    Realized all problems, techniques and refactored all code

---
+++ model.py
@@ -0,0 +1,19 @@
+from typing import TypeVar, Optional
+from pydantic import BaseModel, Field, EmailStr
+
+
+T = TypeVar('T')
+
+class User(BaseModel):
+	username : str = Field(default=None)
+	fullname : str = Field(default=None)
+	email : EmailStr = Field(default=None)
+	hashed_password : str = Field(default=None)
+	root : bool = Field(default=False)
+
+
+class UpdateUser(BaseModel):
+	username : Optional[str]
+	fullname : Optional[str]
+	email : Optional[EmailStr]
+	root : Optional[bool]
